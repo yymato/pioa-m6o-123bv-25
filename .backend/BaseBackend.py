6ff838a2f27ac5749782eb09d06e2c6ea79c0377
SEGatilov <segatilov@mai.education>
2026-03-03 09:11:04 +0300

    add DB backend
    Implemented the Table class, which contains the data
    
    the DataBase class is implemented, which works with tables inside the database.

---
+++ .backend/BaseBackend.py
@@ -0,0 +1,119 @@
+class DataBase:
+    def __init__(self, name):
+        self.tables = {}
+        self.name = name
+
+    def add_table(self, table, table_name: str):
+        if isinstance(table, Table) and isinstance(table_name, str):
+            if table_name in self.tables:
+                raise Exception(f"Table {table_name} already exists")
+            else:
+                self.tables[table_name] = table
+        else:
+            raise Exception("Unknown format: {} {}".format(type(table_name), type(table)))
+
+    def create_table(self, table_name: str="unknown", **table_header):
+        table = Table(**table_header)
+        self.tables[table_name] = table
+
+    def select_from(self, table_name: str, **filter_cols):
+        if table_name in self.tables:
+            return self.tables[table_name].select(**filter_cols)
+        else:
+            raise Exception("Unknown table: {}".format(table_name))
+
+    def delete_rows(self, table_name: str, **filter_cols):
+        if table_name in self.tables:
+            self.tables[table_name].delete(**filter_cols)
+        else:
+            raise Exception("Unknown table: {}".format(table_name))
+
+    def add_row(self, table_name, **cols):
+        if table_name in self.tables:
+            self.tables[table_name].add(**cols)
+        else:
+            raise Exception("Unknown table: {}".format(table_name))
+
+    def update_rows(self, table_name: str, filter_col_name, filter_col_value, **cols):
+        if table_name in self.tables:
+            self.tables[table_name].update(filter_col_value, filter_col_name, **cols)
+        else:
+            raise Exception("Unknown table: {}".format(table_name))
+
+    def get_names_tables(self):
+        return list(self.tables.keys())
+
+    def set_table_name(self, old_table_name, new_table_name):
+        self.tables[old_table_name] = new_table_name
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+
+class Table:
+    def __init__(self, **table_header):
+        self.table_header = {}
+        self.matrix_data = []
+        self.rows_len = 0
+
+        for i, col_name, col_property in enumerate(table_header.items()):
+            col_property["index"] = i
+            self.table_header[col_name] = col_property
+            self.rows_len += 1
+
+    def select(self, **filter_cols):
+        result = []
+        for col_name, value in filter_cols.items():
+            if col_name in self.table_header:
+                index = self.table_header[col_name]["index"]
+                for row in self.matrix_data:
+                    if row[index] == self.check_type(col_name, value):
+                        result.append(row)
+
+        return result
+
+    def add(self, **cols):
+        row = [None for _ in range(self.rows_len)]
+        for col_name, value in cols.items():
+            if col_name in self.table_header:
+                col_index = self.table_header[col_name]["index"]
+                row[col_index] = self.check_type(col_name, value)
+            else:
+                raise Exception("Unknown column: {}".format(col_name))
+        self.matrix_data.append(row)
+
+
+    def delete(self, **filter_cols):
+        for col_name, value in filter_cols.items():
+            if col_name in self.table_header:
+                index = self.table_header[col_name]["index"]
+                for row in self.matrix_data:
+                    if row[index] == self.check_type(col_name, value):
+                        self.matrix_data.remove(row)
+            else:
+                raise Exception("Unknown column: {}".format(col_name))
+
+    def update(self, filter_col_name, filter_col_value, **cols):
+        if filter_col_name in self.table_header:
+            filter_col_index = self.table_header[filter_col_name]["index"]
+            for row in self.matrix_data:
+                if row[filter_col_index] == self.check_type(filter_col_name["name"], filter_col_value):
+                    for col_name, value in cols.items():
+                        index = self.table_header[col_name]["index"]
+                        row[index] = self.check_type(col_name, value)
+
+        else:
+            raise Exception("Unknown column: {}".format(filter_col_name))
+
+    def check_type(self, col_name: str, value):
+        if isinstance(value, self.table_header[col_name]["type"]):
+            return value
+        else:
+            raise Exception("Unknown type: {}".format(type(value)))
+
+
+
+
