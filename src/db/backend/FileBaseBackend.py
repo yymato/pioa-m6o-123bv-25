2f83ff4abd5f24d4ce7ec3a633810e3e0832284e
SEGatilov <segatilov@mai.education>
2026-04-04 15:09:39 +0300

    add csv FileDataBase

--- src/db/backend/FileBaseBackend.py
+++ src/db/backend/FileBaseBackend.py
@@ -1,4 +1,8 @@
+import ast
+import csv
 import json
+import os
+
 from src.db.backend.BaseBackend import DataBase, Table
 
 
@@ -55,7 +59,60 @@ class FileDataBaseJson(DataBase):
 
 
 class FileDataBaseCSV(DataBase):
-    def __init__(self, path, name='Unknown'):
+    def __init__(self, directory, name='Unknown'):
         super().__init__(name)
-        self.path = path
+        self.directory = directory
+        os.makedirs(os.path.abspath(directory), exist_ok=True)
+
+    def save_db(self):
+        for table_name, table in self.tables.items():
+            data = table.to_dict()
+            matrix = data['matrix_data']
+            header = [[f'{key}: {data['table_header'][key].__repr__()}' for key in table.table_header]]
+            data = header + table.matrix_data
+            print(data)
+            with open(f"{self.directory}/{table_name}.csv", "w+") as f:
+                csv.writer(f).writerows(data)
+
+    def open_db(self):
+        files_paths = []
+        print(os.listdir(self.directory))
+        for path in os.listdir(self.directory):
+            if path.endswith(".csv"):
+                files_paths.append(os.path.join(self.directory, path))
+
+        print(files_paths)
+        for path in files_paths:
+            with open(path, "r") as f:
+                data = csv.reader(f)
+
+                print('{' + ','.join(next(data)) + '}')
+                header = ast.literal_eval('{' + ','.join(next(data)) + '}')
+                data_matrix = list(map(list, data))
+
+                table = Table(from_dict={
+                    'table_header': header,
+                    'matrix_data': data_matrix,
+                    'rows_len': len(header)
+                })
+                self.tables[path.split("\\")[-1][:-4]] = table
+                print(path)
+
+        print(self.tables)
+
+    def create_table(self, *args, **kwargs):
+        super().create_table(*args, **kwargs)
+        self.save_db()
+
+    def insert_into(self, *args, **kwargs):
+        super().insert_into(*args, **kwargs)
+        self.save_db()
+
+    def update_set(self, *args, **kwargs):
+        super().update_set(*args, **kwargs)
+        self.save_db()
+
+    def delete_from(self, table_name, **filter_cols):
+        super().delete_from(table_name, **filter_cols)
+        self.save_db()
 
