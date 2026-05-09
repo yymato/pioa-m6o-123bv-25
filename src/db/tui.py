bacf51666bbf87e3e883495e7a3c9e4190671794
SEGatilov <segatilov@mai.education>
2026-05-09 13:18:06 +0300

    fixed all mistakes

--- src/db/tui.py
+++ src/db/tui.py
@@ -21,8 +21,8 @@ class TUI:
         self.main_loop()
 
     def main_loop(self):
-        try:
-            while True:
+        while True:
+            try:
                 user_input = input()
                 if user_input == '\n' or user_input == '':
                     continue
@@ -42,9 +42,8 @@ class TUI:
                         self._delete(user_input)
                     case 'exit':
                         break
-        except Exception as e:
-            print(e)
-
+            except Exception as e:
+                print(e)
 
     def _create_table(self, user_input):
         if user_input[0].lower() == 'table':
@@ -78,111 +77,75 @@ class TUI:
             raise SyntaxError("Not valid request")
 
     def _insert(self, user_input):
-        if user_input[0].lower() == 'into':
-            table_name = user_input[1]
-            col_names = user_input[2:list(map(lambda s: s.lower(), user_input)).index('values')]
-            col_names = list(map(self.strip_bracket, col_names))
-
-            values = user_input[list(map(lambda s: s.lower(), user_input)).index('values') + 1:]
-            values[0] = values[0][1:]
-            values[-1] = values[-1][:-1]
-            values = ' '.join(values)
-
-            right_bracket, left_bracket = 0, 0
-            is_square_bracket = False
-            is_figure_bracket = False
-            is_circle_bracket = False
-            left_index = 0
-            data = []
-            for i, v in enumerate(values):
-
-                if v == '(':
-                    if not is_circle_bracket:
-                        is_circle_bracket = True
-                        left_bracket = 1
-                        left_index = i
-
-                    else:
-                        left_bracket += 1
-                    continue
-
-                if v == ')':
-                    if not is_circle_bracket:
-                        raise ValueError('Bad request')
-                    else:
-                        right_bracket += 1
-
-                    if right_bracket == left_bracket:
-                        data.append(values[left_index:i + 1])
-                        right_bracket, left_bracket = 0, 0
-                        is_circle_bracket = False
-                    continue
-
-                if v == '[':
-                    if not is_square_bracket:
-                        is_square_bracket = True
-                        left_bracket = 1
-                        left_index = i
-
-                    else:
-                        left_bracket += 1
-                    continue
-
-                if v == ']':
-                    if not is_square_bracket:
-                        raise ValueError('Bad request')
-                    else:
-                        right_bracket += 1
-
-                    if right_bracket == left_bracket:
-                        data.append(values[left_index:i + 1])
-                        right_bracket, left_bracket = 0, 0
-                        is_square_bracket = False
-                    continue
-
-
-                if v == '{':
-                    if not is_figure_bracket:
-                        is_figure_bracket = True
-                        left_bracket = 1
-                        left_index = i
-
-                    else:
-                        left_bracket += 1
-                    continue
-
-
-                if v == '}':
-                    if not is_figure_bracket:
-                        raise ValueError('Bad request')
-                    else:
-                        right_bracket += 1
-
-                    if right_bracket == left_bracket:
-                        data.append(values[left_index:i + 1])
-                        right_bracket, left_bracket = 0, 0
-                        is_square_bracket = False
-
-                    continue
-
-                if is_figure_bracket == is_circle_bracket == is_square_bracket == False:
-                    if len(data) == 0:
-                        data.append(v)
-                    elif not data[-1][-1] in ']}':
-                        data[-1] += v
-
-            values = []
-            for i in data:
-                if not ('[' in i or '{' in i):
-                    values.extend(i.split())
-            values = list(map(ast.literal_eval, values))
-
-            data = {col_name: value for col_name, value in zip(col_names, values)}
-
-            self.db.insert_into(table_name=table_name, **data)
-
-        else:
+        if user_input[0].lower() != 'into':
             raise SyntaxError("Not valid request")
+
+        table_name = user_input[1]
+        lowered = [s.lower() for s in user_input]
+
+        try:
+            values_idx = lowered.index('values')
+        except ValueError:
+            raise SyntaxError("Missing VALUES clause")
+
+        col_names = list(map(self.strip_bracket, user_input[2:values_idx]))
+
+        raw_values = user_input[values_idx + 1:]
+
+        if raw_values[0].startswith('('):
+            raw_values[0] = raw_values[0][1:]
+        if raw_values[-1].endswith(')'):
+            raw_values[-1] = raw_values[-1][:-1]
+
+        values_str = ' '.join(raw_values)
+        tokens = []
+        current = ''
+        depth_paren = 0
+        depth_square = 0
+        depth_curly = 0
+
+        for c in values_str:
+            if c == '(':
+                depth_paren += 1
+                current += c
+            elif c == ')':
+                depth_paren -= 1
+                current += c
+            elif c == '[':
+                depth_square += 1
+                current += c
+            elif c == ']':
+                depth_square -= 1
+                current += c
+            elif c == '{':
+                depth_curly += 1
+                current += c
+            elif c == '}':
+                depth_curly -= 1
+                current += c
+            elif c == ' ':
+                if depth_paren == 0 and depth_square == 0 and depth_curly == 0:
+                    if current:
+                        tokens.append(current)
+                        current = ''
+                else:
+                    current += c
+            else:
+                current += c
+
+        if current:
+            tokens.append(current)
+
+        try:
+            values = [ast.literal_eval(tok) for tok in tokens]
+        except (ValueError, SyntaxError) as e:
+            raise ValueError(f"Invalid value literal: {e}")
+
+        if len(col_names) != len(values):
+            raise ValueError("Number of columns does not match number of values")
+
+        data = dict(zip(col_names, values))
+        self.db.insert_into(table_name=table_name, **data)
 
     def _update(self, user_input):
         table_name = user_input[0]
